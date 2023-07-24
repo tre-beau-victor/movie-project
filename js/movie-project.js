f0d0800f7c1fa3ab7c741ad5c626841d8e9cde82
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-24 15:38:23 -0500

    added to js

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,14 +1,25 @@
 "use strict";
 
-const DOMAIN = "http://localhost:3000";
+const DOMAIN = " http://localhost:3000/movies";
 
-const getID = async () => {
-    const response = await fetch(`${DOMAIN}/id`);
-    const id = await response.json();
-    return id;
+const getMovie = async (id) => {
+    const response = await fetch(`${DOMAIN}/${id}`);
+    const movie = await response.json();
+    return movie;
 }
 
-
+const createMovie = async (movie) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'applications/json'
+        },
+        body: JSON.stringify(movie)
+    };
+    const response = await fetch(`${DOMAIN}/${movie}`, options);
+    const apiResponse = response.json();
+    return apiResponse;
+}
 
 
 
