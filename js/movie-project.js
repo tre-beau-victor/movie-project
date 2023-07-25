10dd7e795b596936e77aa9bba7a43e9303dbd455
Braden Wittkop <bradenjwittkop@gmail.com>
2023-07-25 16:28:19 -0500

    finished functionality

--- js/movie-project.js
+++ js/movie-project.js
@@ -49,25 +49,24 @@ const createMovie = async (movie) => {
 
 
 
-const editMovie = async (id) => {
-    const options = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify()
+    const editMovie = async (id, title, genre, rating, reviews) => {
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ title, genre, rating, reviews }) // Include "reviews" property in the request body
+        };
+        const response = await fetch(`${DOMAIN}/${id}`, options);
+        const apiResponse = response.json();
+        return apiResponse;
     };
-    const response = await fetch(`${DOMAIN}/${id}`, options);
-    const apiResponse = response.json();
-    return apiResponse;
-};
 
 
 
 
 
-
-const deleteMovie = async (id) => {
+    const deleteMovie = async (id) => {
     const options = {
         method: 'DELETE',
         headers: {
@@ -82,43 +81,116 @@ const deleteMovie = async (id) => {
 
 
 
-const renderMovie = async (movies) => {
-    document.querySelector('#movies').innerHTML = ""
-    const movieDiv = document.querySelector('#movies');
-    movies.forEach(movie => {
-        const card = document.createElement('div');
-        card.classList.add('movie-card');
-        card.innerHTML = `
-         <img src=""/>
-        <h2>${movie.title}</h2>
-        <h5>${movie.genre}</h5>
-        <h5>${movie.rating}</h5>
-        <div class="review">
+
+    const renderMovie = async (movies) => {
+        document.querySelector('#movies').innerHTML = "";
+        const movieDiv = document.querySelector('#movies');
+
+        movies.forEach(movie => {
+            console.log(movie)
+            const card = document.createElement('div');
+            card.classList.add('movie-card');
+            card.innerHTML = `
+            <img src=""/>
+            <h2>${movie.title}</h2>
+            <h5>${movie.genre}</h5>
+            <h5>${movie.rating}</h5>
+            <h5>${movie.reviews ? movie.reviews.map((movie) => {return `<h5>${movie}</h5>`}).join("") : "No Reviews Yet"}</h5>
+            <div class="review">
                 <textarea class="review-input" placeholder="Enter your review"></textarea>
                 <button class="btn submit-review">SUBMIT</button>
             </div>
-            <button class="btn delete">DELETE</button>
-            <div class="movie-review">${movie.review || ""}</div>
-    `;
-        const deleteBtn = card.querySelector('.btn.delete');
-        deleteBtn.addEventListener('click', async () => {
-            await deleteMovie(movie.id);
-            card.remove();
+            <div class="movie-reviews">
+                ${movie.review ? `<div class="movie-review">${movie.review}</div>` : ''}
+            </div>
+            
+            <button class="btn delete-reviews">DELETE REVIEWS</button>
+            <button class="btn delete-movie">DELETE MOVIE</button>
+        `;
+
+            const deleteMovieBtn = card.querySelector('.btn.delete-movie');
+            deleteMovieBtn.addEventListener('click', async () => {
+                await deleteMovie(movie.id);
+                card.remove();
+            });
+
+            const submitBtn = card.querySelector('.btn.submit-review');
+            submitBtn.addEventListener('click', async () => {
+                const reviewInput = card.querySelector('.review-input');
+                const review = reviewInput.value.trim();
+
+                if (review !== "") {
+                    const movieReviewsDiv = card.querySelector('.movie-reviews');
+                    const newReviewDiv = document.createElement('div');
+                    newReviewDiv.classList.add('movie-review');
+                    newReviewDiv.textContent = review;
+                    movieReviewsDiv.appendChild(newReviewDiv);
+                    reviewInput.value = "";
+
+
+                    // Get the movie ID from the parent card element
+                    const movieId = movie.id;
+
+                    // Update the reviews in the movies array
+                    const movieIndex = movies.findIndex(movie => movie.id === movieId);
+                    if (movieIndex !== -1) {
+                        if (!movies[movieIndex].reviews) {
+                            movies[movieIndex].reviews = []; // Initialize the "reviews" array if not present
+                        }
+                        movies[movieIndex].reviews.push(review); // Add the new review to the "reviews" array
+                    }
+
+                    // Call the editMovie function to update the reviews in the JSON data
+                    const {title, genre, rating, reviews} = movies[movieIndex];
+                    await editMovie(movieId, title, genre, rating, reviews);
+                }
+            });
+
+
+            const deleteReviewsBtn = card.querySelector('.btn.delete-reviews');
+            deleteReviewsBtn.addEventListener('click', async () => {
+                const movieReviewsDiv = card.querySelector('.movie-reviews');
+                movieReviewsDiv.innerHTML = ""; // Clear all reviews for this movie
+                await deleteReviewsBtn(movie.reviews)
+            });
+
+
+
+            document.querySelector('#movies').appendChild(card);
         });
-        const submitBtn = card.querySelector('.btn.submit-review');
-        submitBtn.addEventListener('click', async () => {
-            const reviewInput = card.querySelector('.review-input')
-            const review = reviewInput.value;
-            // Here you can add code to save the review to your backend or do something else with it.
-            movie.review = review; // Save the review in the movie object
-            const movieReviewDiv = card.querySelector('.movie-review');
-            movieReviewDiv.textContent = review; // Display the review in the card
-            reviewInput.value = "";
-        });
-        document.querySelector('#movies').appendChild(card);
-        return card
+    }
+
+
+
+    // function for user to add movies
+
+    document.getElementById("addMovieForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const titleInput = document.getElementById("title");
+        const genreInput = document.getElementById("genre");
+        const ratingInput = document.getElementById("rating");
+        const reviewInput = document.getElementById("review")
+        const newMovie = {
+            title: titleInput.value,
+            genre: genreInput.value,
+            rating: parseFloat(ratingInput.value),
+            review: reviewInput.value,
+        };
+        // console.log(reviewInput.value)
+        // Add the new movie to the movies array
+        movies.push(newMovie);
+        renderMovie(movies);
+        createMovie(newMovie);
+
+        // Optional: You can display the added movie in a list or perform any other action here
+
+        // Clear the input fields
+        titleInput.value = "";
+        genreInput.value = "";
+        ratingInput.value = "";
+        reviewInput.value = "";
     });
-}
 
 
 
@@ -126,18 +198,6 @@ const renderMovie = async (movies) => {
 
     renderMovie(movies);
 
-
-//     const filterMovies = (movies, search) => {
-//         console.log(movies)
-//         return movies.filter(movie => movie.title.includes(search))
-//     }
-//
-// // const filterMovies = (movies, search) => {console.log(movies)
-// //     return movies.filter(movie => movie.title.includes(search))}
-//
-//  const filteredMovies = filterMovies(movies, searchQuery)
-//     console.log(searchQuery)
-//     console.log(filteredMovies);
 
     const filterMovies = (movies, search) => {
         console.log(movies)
@@ -149,29 +209,7 @@ const renderMovie = async (movies) => {
 
 
 
-
-
-//     document.querySelector('#movies').appendChild(card);
-//     return card;
-// }
-
-
-// const renderEditMovie = async (movie) => {
-//     const
-// }
-
-
 renderMovie(movies)
-
-
-///////////////////////////////// IFFE ///////////////////
-
-
-
-
-
-
-
 
 
 
